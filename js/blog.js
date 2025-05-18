8665c4ac5dd6b27b81318acac11750f0e8213b89
Abdulrahaman Ridwan Ayinla <abdulrahamanr657@gmail.com>
2025-05-18 04:53:56 -0700

    Add: trends post section

--- js/blog.js
+++ js/blog.js
@@ -8,9 +8,26 @@ function showPage(page) {
     const start = (page - 1) * itemsPerPage;
     const end = start + itemsPerPage
 
+    //Hide item with animation
     items.forEach((item, i) => {
-        item.style.display = i >= start && i < end ? 'block' : 'none';
+        // item.classList.remove('show');
+        // setTimeout(() => item.style.display = 'none', 400);
+        // item.style.display = i >= start && i < end ? 'block' : 'none';
     });
+
+    // show item with animation after a short delay
+    setTimeout(() => {
+        items.forEach((item, i) => {
+            if (i >= start && i < end) {
+                item.style.display = 'block';
+                setTimeout(() => item.classList.add('show'), 60)
+            }else {
+                item.style.display = 'none'
+                setTimeout(() => item.classList.remove('show'), 60)
+            }
+            
+        })
+    })
 
     document.getElementById('page-number').innerText = page;
 }
